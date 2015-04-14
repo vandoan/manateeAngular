f7cd9ef3102ba067f517f388948c7cae261a90e1
Van Doan <vdoan09@gmail.com>
2015-04-13 23:42:24 -0400

    directives

--- MainController.js
+++ MainController.js
@@ -1,4 +1,11 @@
 app.controller('MainController', ['$scope',
 function($scope) {
-	$scope.title = 'Top Sellers in Books';
+	$scope.title = 'hotdogs';
+	$scope.promo = 'drinks';
+
+	$scope.product = {
+	name: 'The Book of Trees',
+	price:19
+	}
+
 	}]);
