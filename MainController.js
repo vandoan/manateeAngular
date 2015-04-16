48cb3fd92801fe5bf3f8b3763f9cf8d089200050
Van Doan <vdoan09@gmail.com>
2015-04-15 23:05:32 -0400

    loop

--- MainController.js
+++ MainController.js
@@ -3,10 +3,20 @@ function($scope) {
 	$scope.title = 'hotdogs';
 	$scope.promo = 'drinks';
 
-	$scope.product = {
-	name: 'The Book of Trees',
-	price:19,
-	pubdate: new Date('2014', '03', '08')
-	}
+	$scope.products = 
+	[ 
+  { 
+    name: 'The Book of Trees', 
+    price: 19, 
+    pubdate: new Date('2014', '03', '08'), 
+    cover: 'img/the-book-of-trees.jpg' 
+  }, 
+  { 
+    name: 'Program or be Programmed', 
+    price: 8, 
+    pubdate: new Date('2013', '08', '01'), 
+    cover: 'img/program-or-be-programmed.jpg' 
+  } 
+]
 
 	}]);
