2beda59a022f73709aea72f63cbe5dff8d1235a6
Van Doan <vdoan09@gmail.com>
2015-04-17 23:48:06 -0400

    ng-click

--- MainController.js
+++ MainController.js
@@ -9,14 +9,35 @@ function($scope) {
     name: 'The Book of Trees', 
     price: 19, 
     pubdate: new Date('2014', '03', '08'), 
-    cover: 'img/the-book-of-trees.jpg' 
+    cover: 'img/the-book-of-trees.jpg',
+    likes: 0,
+    dislikes: 0,
   }, 
   { 
     name: 'Program or be Programmed', 
     price: 8, 
     pubdate: new Date('2013', '08', '01'), 
-    cover: 'img/program-or-be-programmed.jpg' 
-  } 
+    cover: 'img/program-or-be-programmed.jpg',
+    likes: 0,
+    dislikes: 0,
+  },
+    {
+  	name: 'tao of pooh',
+  	price: 22,
+  	likes: 0,
+  	dislikes: 0,
+  },
+  {
+  	name: '12 habits of highly effective people', 
+  	price: 11,
+  	likes: 0,
+  	dislikes: 0,
+  	}
 ]
-
+	$scope.plusOne = function(index) {
+		$scope.products[index].likes += 1;
+		};
+	$scope.minusOne = function(index) {
+		$scope.products[index].dislikes += 1;
+	};
 	}]);
